580b94be3b4c8be3291eae9888cf403f269f0d42
Felipe O'Donnell <felipe.odonnell@outlook.com>
2023-02-24 17:10:14 +0000

    Add files via upload

---
+++ main copy.py
@@ -0,0 +1,19 @@
+
+
+
+with st.sidebar: 
+    st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
+    st.title("Blueblocks.ai")
+    choice = st.radio("Navigation", ["Valuation","Structuring","Swaps"])
+    st.info("This project application helps you get instant valuations and structures solutions for your property")
+
+if choice == "Valuation":
+    pass
+
+
+
+if choice == "Structuring": 
+    pass
+
+if choice == "Swaps": 
+    pass
